b249342a102f951a1cd61684d057e87a6f4e2958
Eduardo Davis <64754426+denialos18@users.noreply.github.com>
2021-08-15 01:58:17 +0000

    atualizacao de codigos

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello world")
+nomeCompleto = "Eduardo Davis"
+apelido = "mens"
+idade = 30
+dataNascimento = "25/11/1990"
+localNascimento = "Araruna"
+altura = 1.70
+trabalhando = true
+
+apresentacao = "Meu nome é " + nomeCompleto + " (sou conhecido[a] como " + apelido + ") e tenho " + idade + " anos. Nasci no dia " + dataNascimento + ", na cidade de " + localNascimento + ". Tenho " + altura + "m de altura e atualmente estou " + (trabalhando ? "empregado" : "desempregado") + "."
+
+console.log(apresentacao);
